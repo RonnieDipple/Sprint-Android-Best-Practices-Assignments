3f2ad7014a3601065c4178051efa7a9a3a5f1af5
ronniedipple <ronniedipple@gmail.com>
2019-10-31 19:09:57 -0500

    ViewModel updated

--- M04/Assignment/Dagger2Assignment/app/src/main/java/com/example/dagger2assignment/Base/BaseViewModel.kt
+++ M04/Assignment/Dagger2Assignment/app/src/main/java/com/example/dagger2assignment/Base/BaseViewModel.kt
@@ -1,8 +1,31 @@
 package com.example.dagger2assignment.Base
 
 import androidx.lifecycle.ViewModel
+import com.example.dagger2assignment.injection.component.DaggerViewModelInjector
+import com.example.dagger2assignment.injection.component.ViewModelInjector
+import com.example.dagger2assignment.injection.module.NetworkModule
+import com.example.dagger2assignment.ui.post.PostListViewModel
 
 //This will be used for dependency injection
 //Must be abstract so it can be inherited from
 abstract class BaseViewModel: ViewModel() {
+//DaggerViewModelInjector needs to be generated, you do that by REBUILDING THE PROJECT
+
+    //Setup
+    private val injector: ViewModelInjector = DaggerViewModelInjector
+        .builder()
+        .networkModule(NetworkModule)
+        .build()
+    //Method
+    private fun inject(){
+        when(this){
+            is PostListViewModel -> injector.inject(this)
+        }
+    }
+    //Execution
+    init {
+        inject()
+    }
+
+
 }
--- M04/Assignment/Dagger2Assignment/app/src/main/java/com/example/dagger2assignment/ui/post/PostListViewModel.kt
+++ M04/Assignment/Dagger2Assignment/app/src/main/java/com/example/dagger2assignment/ui/post/PostListViewModel.kt
@@ -2,10 +2,53 @@ package com.example.dagger2assignment.ui.post
 
 import com.example.dagger2assignment.Base.BaseViewModel
 import com.example.dagger2assignment.Network.PostApi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class PostListViewModel: BaseViewModel(){
 //in order to get the result from API. This instance will be injected by Dagger
     @Inject
     lateinit var postApi: PostApi
+//All this gets the results
+    private lateinit var subscription: Disposable
+
+    init {
+        loadPosts()
+    }
+
+    private fun loadPosts(){
+        subscription = postApi.getposts()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrievePostListStart()}
+            .doOnTerminate{ onRetrievePostListFinish()}
+            .subscribe({ onRetrievePostListSuccess()},
+                {onRetrievePostListError()}
+
+            )
+    }
+
+    private fun onRetrievePostListStart(){
+
+    }
+
+    private fun onRetrievePostListFinish(){
+
+    }
+
+    private fun onRetrievePostListSuccess(){
+
+    }
+
+    private fun onRetrievePostListError(){
+
+    }
+
+    //when the view model is no longer in use it should be disposed of 
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
 }
